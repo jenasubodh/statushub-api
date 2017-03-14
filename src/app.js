9329c5a5bc235770a256af90cc4c04cdca5849c0
Subodh Jena <jenasubodh@gmail.com>
2017-03-15 01:17:38 +0530

    Creates Initial Socket Connection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import socketIO from 'socket.io';
 import { env, mongo, port, ip } from './config'
 import mongoose from './services/mongoose'
 import express from './services/express'
@@ -6,6 +7,15 @@ import api from './api'
 
 const app = express(api)
 const server = http.createServer(app)
+
+const io = new socketIO(server);
+io.on('connection', (socket) => {
+  console.log("User Connected !!")
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+})
 
 mongoose.connect(mongo.uri)
 
